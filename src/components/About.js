dbf12d095db57e8141c774e36ef317b273279366
tillenotuoma <totuoma27@gmail.com>
2025-01-17 16:24:41 +0300

    updated components

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a front-end developer with a particular interest in making things
+        simple and automating daily tasks which can result in saving much time
+        and prevent errors.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
